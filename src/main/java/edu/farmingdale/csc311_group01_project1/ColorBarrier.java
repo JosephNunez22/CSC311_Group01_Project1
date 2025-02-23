dfa9f8b15b6cdff409d933312844d2b28cdf9d9a
EduardoEsc0bar <eduardoescobar0621@gmail.com>
2025-02-22 21:06:09 -0500

    fixed 11,14 problem

--- src/main/java/edu/farmingdale/csc311_group01_project1/ColorBarrier.java
+++ src/main/java/edu/farmingdale/csc311_group01_project1/ColorBarrier.java
@@ -12,38 +12,37 @@ public class ColorBarrier {
     private Color barrierColor;
     private int maxRow;
     private int maxCol;
+    private static final Color PATH_COLOR = Color.WHITE; // 0xffffffff
+    private static final int Y_OFFSET = 5; // Add a small offset to align with grid
 
     public ColorBarrier(Image mazeImage, int barrierColorX, int barrierColorY, int maxRow, int maxCol) {
         this.mazeImage = mazeImage;
         this.maxRow = maxRow;
         this.maxCol = maxCol;
+        this.barrierColor = Color.BLACK;
 
-
-        //scans image for color barrier
-        PixelReader pixelReader = mazeImage.getPixelReader();
-        if(barrierColorX > 0 && barrierColorX < mazeImage.getWidth() && barrierColorY > 0 && barrierColorY < mazeImage.getHeight() ) {
-            this.barrierColor = pixelReader.getColor(barrierColorX, barrierColorY);
-        } else{
-            throw new IllegalArgumentException("barrierColorX or barrierColorY must be out of bounds");
-        }
+        System.out.println("Path color: " + PATH_COLOR);
+        System.out.println("Barrier color: " + barrierColor);
     }
 
-
     public boolean checkBarrier(int row, int col) {
-       if(row >= 0 && row < maxRow && col >= 0 && col < maxCol){
-           int pixelX = col * (int)(mazeImage.getWidth()/maxCol);
-           int pixelY = row * (int)(mazeImage.getHeight()/maxRow);
+        if(row >= 0 && row < maxRow && col >= 0 && col < maxCol) {
+            double cellWidth = mazeImage.getWidth() / maxCol;
+            double cellHeight = mazeImage.getHeight() / maxRow;
 
-           System.out.println("checking barriers at pixelX: " + pixelX + " pixelY: " + pixelY);
+            int pixelX = (int)(col * cellWidth);
+            int pixelY = (int)(row * cellHeight) + Y_OFFSET; // Add Y_OFFSET here
 
-           PixelReader pixelReader = mazeImage.getPixelReader();
-           if(pixelX >= 0 && pixelX < mazeImage.getWidth() && pixelY >= 0 && pixelY < mazeImage.getHeight()){
-               Color color = pixelReader.getColor(pixelX,pixelY);
-               System.out.println("Barrier color: " + barrierColor + ", Current color: " + color);
-               return !color.equals(barrierColor);
-           }
+            System.out.println("Checking position at pixelX: " + pixelX + " pixelY: " + pixelY);
 
-       }
-       return true;
+            PixelReader pixelReader = mazeImage.getPixelReader();
+            if(pixelX >= 0 && pixelX < mazeImage.getWidth() &&
+                    pixelY >= 0 && pixelY < mazeImage.getHeight()) {
+                Color color = pixelReader.getColor(pixelX, pixelY);
+                System.out.println("Color at position: " + color);
+                return color.equals(PATH_COLOR);
+            }
+        }
+        return false;
     }
 }
--- src/main/java/edu/farmingdale/csc311_group01_project1/HardMapController.java
+++ src/main/java/edu/farmingdale/csc311_group01_project1/HardMapController.java
@@ -10,12 +10,12 @@ import javafx.scene.paint.Color;
 public class HardMapController {
 
     /*Starting position
-    *
-    * **/
+     *
+     * **/
     private int startPlayerRow = 9;
     private int startPlayerCol = 1;
-    private final int MAX_ROW = 14;
-    private final int MAX_COL = 21;
+    private final int MAX_ROW = 15;
+    private final int MAX_COL = 22;
     private ColorBarrier colorBarrier;
 
     private int playRow = startPlayerRow;
@@ -31,53 +31,62 @@ public class HardMapController {
     private ImageView mapImage;
 
     @FXML
-    private void initialize(){
+    private void initialize() {
         Image mazeImage = mapImage.getImage();
-        colorBarrier = new ColorBarrier(mazeImage, 29, 300, MAX_ROW, MAX_COL) ;
+        colorBarrier = new ColorBarrier(mazeImage, 29, 300, MAX_ROW, MAX_COL);
 
-        GridPane.setRowIndex(robotImage,playRow);
-        GridPane.setColumnIndex(robotImage,playCol);
+        GridPane.setRowIndex(robotImage, playRow);
+        GridPane.setColumnIndex(robotImage, playCol);
         mazeGrid.requestFocus();
     }
+
     @FXML
     void navigateIcon(KeyEvent e) {
         int nextRow = playRow;
         int nextCol = playCol;
 
-        switch(e.getCode()){
-            case UP: nextRow--;
+        switch (e.getCode()) {
+            case UP:
+                nextRow--;
                 break;
-            case DOWN: nextRow++;
+            case DOWN:
+                nextRow++;
                 break;
-            case LEFT: nextCol--;
+            case LEFT:
+                nextCol--;
                 break;
-            case RIGHT: nextCol++;
+            case RIGHT:
+                nextCol++;
                 break;
-            default: return;
+            default:
+                return;
         }
 
         System.out.println("Attempting to move to row: " + nextRow + " col: " + nextCol);
-        if(nextCol == MAX_COL && (nextRow == 8||nextRow == 9)){
+
+        // Check for win condition
+        if (nextCol == MAX_COL && (nextRow == 8 || nextRow == 9)) {
             System.out.print("you win");
-            GridPane.setRowIndex(robotImage,playRow);
-            GridPane.setColumnIndex(robotImage,playCol);
-        } else if (nextRow > 0 && nextRow <= MAX_ROW && nextCol > 0 && nextCol <= MAX_COL){
-            if(colorBarrier.checkBarrier(nextRow, nextCol)){
+            GridPane.setRowIndex(robotImage, playRow);
+            GridPane.setColumnIndex(robotImage, playCol);
+        }
+        // Check if move is within bounds
+        else if (nextRow > 0 && nextRow <= MAX_ROW && nextCol > 0 && nextCol <= MAX_COL) {
+            if (colorBarrier.checkBarrier(nextRow, nextCol)) {
+                // Valid move - update position
                 playRow = nextRow;
                 playCol = nextCol;
-                GridPane.setRowIndex(robotImage,playRow);
-                GridPane.setColumnIndex(robotImage,playCol);
-            }else{
-                System.out.print("oops, try again");
-                playRow = startPlayerRow;
-                playCol = startPlayerCol;
-                GridPane.setRowIndex(robotImage,playRow);
-                GridPane.setColumnIndex(robotImage,playCol);
-
+                GridPane.setRowIndex(robotImage, playRow);
+                GridPane.setColumnIndex(robotImage, playCol);
+            } else {
+                // Hit barrier - stay in current position
+                System.out.println("Hit a barrier - can't move there");
+                // Don't reset to start, just stay where we are
             }
-        }else{
-            System.out.print("invalid move: row: " + nextRow + ", col: " + nextCol);
+        } else {
+            // Out of bounds - stay in current position
+            System.out.println("invalid move: row: " + nextRow + ", col: " + nextCol);
+            // Don't reset to start, just stay where we are
         }
     }
-
 }
